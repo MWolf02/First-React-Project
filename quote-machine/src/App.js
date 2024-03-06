99bbe38279f8d71c584b7532f3786a6473fc5a6e
Martin Wolf <martinwolflauring@gmail.com>
2024-03-06 09:41:32 +0100

    App working - no css

--- quote-machine/src/App.js
+++ quote-machine/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+
+  useEffect(() => {
+    fetchQuote();
+  }, []); // Empty dependency array means this effect runs once after the first render
+
+  const fetchQuote = async () => {
+    const url = 'https://andruxnet-random-famous-quotes.p.rapidapi.com/?cat=famous&count=1';
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '1bb2e5e5a5msh8c3fcf17d45c517p1dc9acjsneb6a55bcdcbc',
+        'X-RapidAPI-Host': 'andruxnet-random-famous-quotes.p.rapidapi.com'
+      }
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+      setQuote(data[0].quote);
+      setAuthor(data[0].author);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleNewQuote = () => {
+    fetchQuote();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="quote-box">
+      <p id="text">{quote}</p>
+      <p id="author">- {author}</p>
+      <button id="new-quote" onClick={handleNewQuote}>New Quote</button>
+      <a id="tweet-quote" href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(`"${quote}" - ${author}`)}`} target="_blank" rel="noopener noreferrer">Tweet Quote</a>
     </div>
   );
 }
