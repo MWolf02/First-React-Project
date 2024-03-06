4b255cb3105c90ad2d6b45a2caab1301c0ffbf6b
Martin Wolf <martinwolflauring@gmail.com>
2024-03-06 11:49:37 +0100

    Quote machine - first draft

--- quote-machine/src/App.js
+++ quote-machine/src/App.js
@@ -1,22 +1,26 @@
-import React, { useEffect, useState } from "react";
-import './App.css';
+import React, { useEffect, useState } from "react"; 
+import "./App.css";
+import TwitterIcon from "./icon.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
-  const [quote, setQuote] = useState('');
-  const [author, setAuthor] = useState('');
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
 
   useEffect(() => {
     fetchQuote();
   }, []); // Empty dependency array means this effect runs once after the first render
 
   const fetchQuote = async () => {
-    const url = 'https://andruxnet-random-famous-quotes.p.rapidapi.com/?cat=famous&count=1';
+    const url =
+      "https://andruxnet-random-famous-quotes.p.rapidapi.com/?cat=famous&count=1";
     const options = {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'X-RapidAPI-Key': '1bb2e5e5a5msh8c3fcf17d45c517p1dc9acjsneb6a55bcdcbc',
-        'X-RapidAPI-Host': 'andruxnet-random-famous-quotes.p.rapidapi.com'
-      }
+        "X-RapidAPI-Key": "1bb2e5e5a5msh8c3fcf17d45c517p1dc9acjsneb6a55bcdcbc",
+        "X-RapidAPI-Host": "andruxnet-random-famous-quotes.p.rapidapi.com",
+      },
     };
 
     try {
@@ -35,10 +39,26 @@ function App() {
 
   return (
     <div className="App" id="quote-box">
-      <p id="text">{quote}</p>
+      <div className="quote-text">
+        <FontAwesomeIcon icon={faQuoteLeft} />
+        <p id="text">{quote}</p>
+      </div>
       <p id="author">- {author}</p>
-      <button id="new-quote" onClick={handleNewQuote}>New Quote</button>
-      <a id="tweet-quote" href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(`"${quote}" - ${author}`)}`} target="_blank" rel="noopener noreferrer">Tweet Quote</a>
+      <div className="buttons">
+        <button id="new-quote" onClick={handleNewQuote}>
+          New Quote
+        </button>
+        <a
+          id="tweet-quote"
+          href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
+            `"${quote}" - ${author}`
+          )}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="tweet-icon" src={TwitterIcon} alt="search"></img>
+        </a>
+      </div>
     </div>
   );
 }
